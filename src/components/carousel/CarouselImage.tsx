eac89c4a1ef4b03b3ef4dda6ea3579491e944e64
ashishkpaul <ashish.kpaul@gmail.com>
2023-11-30 10:25:49 +0530

    On Carousel showing collection.name at the bottom

--- src/components/carousel/CarouselImage.tsx
+++ src/components/carousel/CarouselImage.tsx
@@ -32,25 +32,17 @@ export default component$(({ collection }: IProps) => {
 	};
 
 	// Check if carouselImage or featuredAsset is defined
-	if (carouselImage) {
+	if (carouselImage || featuredAsset) {
 		return (
 			<Link href={`/collections/${collection.slug}`} key={collection.id}>
 				<div class="relative rounded-lg overflow-hidden hover:opacity-75 xl:w-auto mx-auto h-96">
-					<div class="w-full h-full object-center object-cover">{renderImage(carouselImage)}</div>
+					<div class="w-full h-full object-center object-cover">
+						{carouselImage
+							? renderImage(carouselImage)
+							: featuredAsset && renderImage(featuredAsset)}
+					</div>
 					<span class="absolute w-full bottom-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50" />
-					<span class="absolute w-full bottom-2 mt-auto text-center text-xl font-bold text-white">
-						{collection.name}
-					</span>
-				</div>
-			</Link>
-		);
-	} else if (featuredAsset) {
-		return (
-			<Link href={`/collections/${collection.slug}`} key={collection.id}>
-				<div class="relative rounded-lg overflow-hidden hover:opacity-75 xl:w-auto mx-auto">
-					<div class="w-full h-full object-center object-cover">{renderImage(featuredAsset)}</div>
-					<span class="absolute w-full bottom-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50" />
-					<span class="absolute w-full bottom-2 mt-auto text-center text-xl font-bold text-white">
+					<span class="absolute w-full bottom-2 mt-auto text-center text-xl font-bold text-white sm:text-base">
 						{collection.name}
 					</span>
 				</div>
@@ -60,8 +52,25 @@ export default component$(({ collection }: IProps) => {
 		// If both carouselImage and featuredAsset are undefined, show a random image
 		const renderRandomImage = () => {
 			// Your logic for rendering a random image goes here
-			// You can use Math.random() or any other method to choose a random image
-			return <div>Random Image</div>;
+			// You can use Math.random() to choose a random index from an array of image URLs
+			const randomImages = [
+				'http://example.com/random-image1.jpg',
+				'http://example.com/random-image2.jpg',
+				'http://example.com/random-image3.jpg',
+				// Add more image URLs as needed
+			];
+			const randomIndex = Math.floor(Math.random() * randomImages.length);
+			const randomImageUrl = randomImages[randomIndex];
+
+			return (
+				<Image
+					layout="fullWidth"
+					loading="lazy"
+					class="w-full h-full object-center object-cover"
+					src={randomImageUrl}
+					alt={collection.name}
+				/>
+			);
 		};
 
 		return renderRandomImage();
