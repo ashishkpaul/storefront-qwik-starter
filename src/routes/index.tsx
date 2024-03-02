259036a4bafc9f030903a889a263e93e0d7548b8
ashishkpaul <ashish.kpaul@gmail.com>
2024-03-02 13:18:23 +0530

    On home page fetch products from collection

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,55 @@
-import { component$, useContext } from '@builder.io/qwik';
-import CollectionCard from '~/components/collection-card/CollectionCard';
-import { APP_STATE } from '~/constants';
-import Hero from '../components/widgets/Hero';
+// ./src/routes/index.tsx
+import { component$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { Slider } from 'qwik-slider';
+import ProductCard from '~/components/products/ProductCard';
+import {
+	searchQueryWithCollectionSlug,
+	searchQueryWithTerm,
+} from '~/providers/shop/products/products';
+import { cleanUpParams } from '~/utils';
+
+export const useSearchLoader = routeLoader$(async ({ params }) => {
+	const cleanParams = cleanUpParams(params);
+	const activeFacetValueIds: string[] = cleanParams.f?.split('-') || [];
+	const collectionSlug = cleanParams.slug ?? 'electronics'; // Assuming you have access to the collection slug somehow
+
+	return activeFacetValueIds.length
+		? await searchQueryWithTerm(collectionSlug, '', activeFacetValueIds)
+		: await searchQueryWithCollectionSlug(collectionSlug);
+});
 
 export default component$(() => {
-	const collections = useContext(APP_STATE).collections;
+	// const collections = useContext(APP_STATE).collections;
+	const sliderSettingsTwo = {
+		scrollSpeed: 1,
+		autoScroll: true,
+		autoScrollSpeed: 10,
+		gap: 25,
+		// width: '960px',
+		styleClass: 'test',
+	};
+	// Fetch search results
+	const searchSignal = useSearchLoader();
 
+	// Render ProductCard components based on search results
 	return (
-		<>
-			<Hero />
-			<div>
-				<section class="pt-12 xl:max-w-7xl xl:mx-auto xl:px-8">
-					<div class="mt-4 flow-root">
-						<div class="-my-2">
-							<div class="box-content py-2 px-2 relative overflow-x-auto xl:overflow-visible">
-								<div class="sm:px-6 lg:px-8 xl:px-0 pb-4">
-									<h2 class="text-2xl font-light tracking-tight text-gray-900">{$localize`Shop by Category`}</h2>
-								</div>
-								<div class="grid justify-items-center grid-cols-2 md:grid-cols-3 gap-y-8 gap-x-8 sm:px-6 lg:px-8 xl:relative xl:px-0 xl:space-x-0 xl:gap-x-8">
-									{collections.map((collection) =>
-										collection.featuredAsset ? (
-											<CollectionCard key={collection.id} collection={collection} />
-										) : null
-									)}
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
+		<div>
+			<h1>Products in Collection</h1>
+			<div class="bg">
+				<Slider {...sliderSettingsTwo}>
+					{searchSignal.value.items.map((item) => (
+						<ProductCard
+							key={item.productId}
+							productAsset={item.productAsset}
+							productName={item.productName}
+							slug={item.slug}
+							priceWithTax={item.priceWithTax}
+							currencyCode={item.currencyCode}
+						/>
+					))}
+				</Slider>
 			</div>
-		</>
+		</div>
 	);
 });
