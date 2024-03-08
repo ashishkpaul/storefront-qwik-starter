2fa75c5c0a0651ff77b521a9bbab99e86001f9b8
ashishkpaul <ashish.kpaul@gmail.com>
2024-03-08 19:53:13 +0530

    Change slider text

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -56,7 +56,7 @@ export default component$(() => {
 							<div class="max-w-xs sm:max-w-md m-auto flex flex-nowrap flex-col sm:flex-row sm:justify-center gap-4 lg:justify-start lg:m-0 lg:max-w-7xl">
 								<div class="flex w-full sm:w-auto">
 									<a class="btn btn-primary sm:mb-0 w-full" href="#" target="_blank" rel="noopener">
-										Become Online Seller
+										Become a seller
 									</a>
 								</div>
 								<div class="flex w-full sm:w-auto">
