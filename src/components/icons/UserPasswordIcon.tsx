e0de1a5d988b59742ae2e4c23ace88b4988439c2
ashishkpaul <ashish.kpaul@gmail.com>
2024-03-09 16:39:47 +0530

    Update User Password Icon

--- src/components/icons/UserPasswordIcon.tsx
+++ src/components/icons/UserPasswordIcon.tsx
@@ -2,18 +2,27 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
 	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-6 h-6"
-		>
+		// <svg
+		// 	xmlns="http://www.w3.org/2000/svg"
+		// 	fill="none"
+		// 	viewBox="0 0 24 24"
+		// 	stroke-width="1.5"
+		// 	stroke="currentColor"
+		// 	class="w-6 h-6"
+		// >
+		// 	<path
+		// 		stroke-linecap="round"
+		// 		stroke-linejoin="round"
+		// 		d="M12 17v4m-2-1l4-2m-4 0l4 2m-9-3v4m-2-1l4-2m-4 0l4 2m12-3v4m-2-1l4-2m-4 0l4 2M9 6a3 3 0 1 0 6 0a3 3 0 0 0-6 0m-2 8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2/>
+		// </svg>
+		<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
 			<path
+				fill="none"
+				stroke="currentColor"
 				stroke-linecap="round"
 				stroke-linejoin="round"
-				d="M11.5 4a3.5 3.5 0 1 0 0 7a3.5 3.5 0 0 0 0-7M6 7.5a5.5 5.5 0 1 1 11 0a5.5 5.5 0 0 1-11 0M17 12h4v2h-2v2.003h1.953L22.5 17.14v5.36h-9v-5.36l1.547-1.137H17zm-9 4a4 4 0 0 0-4 4h7.55v2H2v-2a6 6 0 0 1 6-6h3.5v2zm7.5 2.152V20.5h5v-2.348l-.203-.15h-4.594z"
+				stroke-width="2"
+				d="M12 17v4m-2-1l4-2m-4 0l4 2m-9-3v4m-2-1l4-2m-4 0l4 2m12-3v4m-2-1l4-2m-4 0l4 2M9 6a3 3 0 1 0 6 0a3 3 0 0 0-6 0m-2 8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2"
 			/>
 		</svg>
 	);
