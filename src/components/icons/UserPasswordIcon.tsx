7a2267e4c3c63e8468cb1b7c8f40163637e732bb
ashishkpaul <ashish.kpaul@gmail.com>
2024-03-10 09:40:33 +0530

    update userpasswordIcon from icons

--- src/components/icons/UserPasswordIcon.tsx
+++ src/components/icons/UserPasswordIcon.tsx
@@ -2,26 +2,13 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
 	return (
-		// <svg
-		// 	xmlns="http://www.w3.org/2000/svg"
-		// 	fill="none"
-		// 	viewBox="0 0 24 24"
-		// 	stroke-width="1.5"
-		// 	stroke="currentColor"
-		// 	class="w-6 h-6"
-		// >
-		// 	<path
-		// 		stroke-linecap="round"
-		// 		stroke-linejoin="round"
-		// 		d="M12 17v4m-2-1l4-2m-4 0l4 2m-9-3v4m-2-1l4-2m-4 0l4 2m12-3v4m-2-1l4-2m-4 0l4 2M9 6a3 3 0 1 0 6 0a3 3 0 0 0-6 0m-2 8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2/>
-		// </svg>
-		<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+		<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24">
 			<path
 				fill="none"
 				stroke="currentColor"
 				stroke-linecap="round"
 				stroke-linejoin="round"
-				stroke-width="2"
+				stroke-width="1.5"
 				d="M12 17v4m-2-1l4-2m-4 0l4 2m-9-3v4m-2-1l4-2m-4 0l4 2m12-3v4m-2-1l4-2m-4 0l4 2M9 6a3 3 0 1 0 6 0a3 3 0 0 0-6 0m-2 8a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2"
 			/>
 		</svg>
